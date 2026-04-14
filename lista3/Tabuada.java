c5b90f3fb726aa4a8d20fcfa7795ef227f0376b6
Yan Barroso <123784025+yanbarroso@users.noreply.github.com>
2026-04-14 22:50:56 +0000

    Add lista3 + EX1

---
+++ lista3/Tabuada.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Tabuada {
+    public static void main(String [] args) {
+      Scanner teclado = new Scanner(System.in); 
+
+      int multiplicador = 1;
+      int multiplicando = 1;
+
+      for (multiplicador = 1; multiplicador <= 10; multiplicador++) {
+        for (multiplicando = 1; multiplicando <=10; multiplicando++) {
+
+        }
+    }
+}
